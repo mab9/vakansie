b512319d7a9ebd09d3de18c9efc211406f69647a
marcantoine <marc-antoine.bruelhart@bedag.ch>
2020-08-16 17:54:51 +0200

    Add logic for new menu structure and adjust menu tests

--- vakansie-frontend/src/menu/menu.js
+++ vakansie-frontend/src/menu/menu.js
@@ -27,6 +27,36 @@ const controller = arg_1 => arg_2 => arg_3 => arg_3;
  */
 const Menu = () => {
 
+    const entries2 = JSON.parse(`{ "data" : [
+                            {
+                              "id":     "0",
+                              "title":  "Home",
+                              "ctrl" :  "HomeController",
+                              "view" :  "HomeView",
+                              "roles":  [],
+                              "rights": [],
+                              "subs":   []
+                            },
+                            {
+                              "id":     "1",
+                              "title":  "Persons",
+                              "ctrl" :  "PersonController",
+                              "view" :  "PersonView",
+                              "roles":  [],
+                              "rights": [],
+                              "subs":   []
+                            },
+                            {
+                              "id":     "2" ,
+                              "title":  "User",
+                              "ctrl" :  "UserController",
+                              "view" :  "UserView",
+                              "roles":  [],
+                              "rights": [],
+                              "subs":   []
+                            }
+                           ]}`);
+
     const entries = [
         Entry('Vakansie')(MENU_HOME_VIEW)("HomeController"),
         Entry('Planner')(MENU_PLANNING_VIEW)("not implemented"),
@@ -35,24 +65,39 @@ const Menu = () => {
 
     // initial entry
     let selectedEntry = Observable(entries[0]);
+    let selectedEntry2 = Observable(entries2.data[0])
 
     const setSelectedEntry = value => {
         const newEntry = entries.find(entry => entry(label) === value)
         selectedEntry.setValue(newEntry);
     }
 
+    const setSelectedEntry2 = value => {
+        // todo change it to use id instead of the title
+        const newEntry = entries2.data.find(entry => entry.title === value)
+        selectedEntry2.setValue(newEntry);
+    }
+
     /**
      * @typedef Menu
      * @property {function} getEntries
+     * @property {function} getEntries2
      * @property {function} getSelectedEntry
      * @property {function} setSelectedEntry
+     * @property {function} setSelectedEntry2
      * @property {function} onSelectedEntryChange
+     * @property {function} onSelectedEntryChange2
      */
     return {
         getEntries: () => entries,
+        getEntries2: () => entries2.data,
         getSelectedEntry: selectedEntry.getValue,
+        getSelectedEntry2: selectedEntry2.getValue,
         setSelectedEntry: setSelectedEntry,
-        onSelectedEntryChange: selectedEntry.onChange
+        setSelectedEntry2: setSelectedEntry2,
+        onSelectedEntryChange: selectedEntry.onChange,
+        onSelectedEntryChange2: selectedEntry2.onChange
+
     }
 }
 
--- vakansie-frontend/src/menu/menu.test.js
+++ vakansie-frontend/src/menu/menu.test.js
@@ -1,51 +1,34 @@
-import { Suite }            from "../test/test.js"
+import {Suite} from "../test/test.js"
+import {Menu} from "./menu.js";
 
 const util = Suite("menu-json");
 
 util.add("Json Menu to JS object", assert => {
-
-    const menuEntries = `{ "data" : [
-                            { "id":"0" , "title":"User" },
-                            { "id":"1" , "title":"Planner" },
-                            { "id":"2" , "title":"License" }
-                           ]}`;
-
-
-    const menu = JSON.parse(menuEntries)
-
-    assert.true(menu);
-    assert.true(menu.data.length === 3);
-
-    assert.is(menu.data[0].id, '0');
-    assert.is(menu.data[0].title, 'User');
-
-
-    assert.is(Object.keys(menu)[0], 'data')
-    assert.is(Object.keys(menu.data[0])[0], 'id')
-    assert.is(Object.keys(menu.data[0])[1], 'title')
-
-    assert.is(menu.data[0].id, '0')
-    assert.is(menu.data[0].title, 'User')
+    const menu = Menu();
+    assert.is(menu.getEntries2()[0].id, '0');
+    assert.is(menu.getEntries2()[0].title, 'Home');
+    assert.is(menu.getEntries2()[0].ctrl, 'HomeController');
 });
 
+util.add("Sorted menu entries", assert => {
+    const menu = Menu();
+    assert.is(menu.getEntries2()[0].id, '0');
+    assert.is(menu.getEntries2()[0].title, 'Home');
 
-util.add("Menu entries sorting", assert => {
-    const menuEntries = `{ "data" : [
-                            { "id":"0" , "title":"User" },
-                            { "id":"1" , "title":"Planner" },
-                            { "id":"2" , "title":"License" }
-                           ]}`;
-
-    const menu = JSON.parse(menuEntries)
-
-    assert.is(menu.data[0].id, '0')
-    assert.is(menu.data[0].title, 'User')
-
-    assert.is(menu.data[1].id, '1')
-    assert.is(menu.data[1].title, 'Planner')
-
-    assert.is(menu.data[2].id, '2')
-    assert.is(menu.data[2].title, 'License')
+    assert.is(menu.getEntries2()[1].id, '1');
+    assert.is(menu.getEntries2()[1].title, 'Persons');
 });
+
+util.add("Selected entry", assert => {
+
+    const menu = Menu();
+    assert.is(menu.getSelectedEntry2().id, '0');
+    assert.is(menu.getSelectedEntry2().title, 'Home');
+
+    menu.setSelectedEntry2('Persons')
+
+    assert.is(menu.getSelectedEntry2().id, '1');
+    assert.is(menu.getSelectedEntry2().title, 'Persons');
+})
 
 util.run();
