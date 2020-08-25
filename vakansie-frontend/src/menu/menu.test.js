ce2dab7f8a8801c310a19e42eaf9d89644c01e57
marcantoine <marc-antoine.bruelhart@bedag.ch>
2020-08-25 16:53:39 +0200

    Fix failing tests
    - Menu entries were changed and
      they did not corespond to the new
      * old test

--- vakansie-frontend/src/menu/menu.test.js
+++ vakansie-frontend/src/menu/menu.test.js
@@ -7,7 +7,7 @@ util.add("Json Menu to JS object", assert => {
     const element = document.createElement("DIV");
     const menu = Menu(element);
     assert.is(menu.getEntries()[0].id, '0');
-    assert.is(menu.getEntries()[0].title, 'Vakansie');
+    assert.is(menu.getEntries()[0].title, 'menu.main.entry.vakansie');
     assert.is(menu.getEntries()[0].ctrl, 'HomeController');
 });
 
@@ -15,22 +15,22 @@ util.add("Sorted menu entries", assert => {
     const element = document.createElement("DIV");
     const menu = Menu(element);
     assert.is(menu.getEntries()[0].id, '0');
-    assert.is(menu.getEntries()[0].title, 'Vakansie');
+    assert.is(menu.getEntries()[0].title, 'menu.main.entry.vakansie');
 
     assert.is(menu.getEntries()[1].id, '1');
-    assert.is(menu.getEntries()[1].title, 'Persons');
+    assert.is(menu.getEntries()[1].title, 'menu.main.entry.persons');
 });
 
 util.add("Selected entry", assert => {
     const element = document.createElement("DIV");
     const menu = Menu(element);
     assert.is(menu.getSelectedEntry().id, '0');
-    assert.is(menu.getSelectedEntry().title, 'Vakansie');
+    assert.is(menu.getSelectedEntry().title, 'menu.main.entry.vakansie');
 
     menu.setSelectedEntry('1')
 
     assert.is(menu.getSelectedEntry().id, '1');
-    assert.is(menu.getSelectedEntry().title, 'Persons');
+    assert.is(menu.getSelectedEntry().title, 'menu.main.entry.persons');
 })
 
 util.run();
