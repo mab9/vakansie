9962679aeceba24e1cb1b349eec9c609a28588d9
marcantoine <marc-antoine.bruelhart@bedag.ch>
2020-08-30 14:56:21 +0200

    Add dom test

--- vakansie-frontend/src/assets/util/dom.test.js
+++ vakansie-frontend/src/assets/util/dom.test.js
@@ -35,4 +35,25 @@ util.add("Convert nested to HTML elements", assert => {
     //  }
 });
 
+util.add("Convert to html and do i18n", assert => {
+    const key = "test.dom.title";
+    const element = dom(`<div><h1 data-i18n="${key}">yeah</h1></div>`);
+
+    // At this moment the i18n should already have changed the inner text.
+    // The translation service was not initalized and therefore the default (i18n key)
+    // will be displayed!
+
+    const div = document.createElement("div")
+    assert.is(element.childElementCount, 1);
+    assert.is(element.childNodes[0].nodeName, div.nodeName);
+    assert.true(element.childNodes[0].innerHTML === `<h1 data-i18n="${key}">${key}</h1>`);
+
+    const child = element.childNodes[0];
+    const h1 = document.createElement("h1")
+
+    assert.is(child.childElementCount, 1);
+    assert.is(child.childNodes[0].nodeName, h1.nodeName);
+    assert.is(child.childNodes[0].innerHTML, key);
+});
+
 util.run();
