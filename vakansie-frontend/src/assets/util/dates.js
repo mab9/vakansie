7f081e1a8520ad1dda5457fd06fba700ee379f93
marcantoine <marc-antoine.bruelhart@bedag.ch>
2020-09-09 14:40:57 +0200

    Add new date functions via prototype

--- vakansie-frontend/src/assets/util/dates.js
+++ vakansie-frontend/src/assets/util/dates.js
@@ -13,6 +13,7 @@ const sameDay = function (date) {
     return left.getFullYear() === date.getFullYear() && left.getMonth() === date.getMonth() && left.getDate() === date.getDate();
 };
 
+// todo rename to isBetween
 const between = function (from, to) {
     guardInstanceOfDate(from);
     guardInstanceOfDate(to);
@@ -21,6 +22,39 @@ const between = function (from, to) {
     return between >= from && between <= to;
 };
 
+const daysBetween = function (right) {
+    guardInstanceOfDate(right);
+    const left = this;
+
+    // The number of milliseconds in all UTC days (no DST).
+    // UTC day always lasts 24 hours, unlike in other time formats, so it's safe do divide by 24 hours
+    const oneDay = 1000 * 60 * 60 * 24;
+
+    // A day in UTC always lasts 24 hours (unlike in other time formats)
+    const start = Date.UTC(left.getFullYear(), left.getMonth(), left.getDate());
+    const end = Date.UTC(right.getFullYear(), right.getMonth(), right.getDate());
+
+    // it's safe to divide by 24 hours
+    return Math.abs((start - end) / oneDay);
+}
+
+const getFormated = function () {
+    const left = this;
+    let year = '' + left.getFullYear();
+    let month = '' + (left.getMonth() + 1);
+    let day = '' + left.getDate();
+
+    if (month.length < 2) {
+        month = '0' + month;
+    }
+    if (day.length < 2) {
+        day = '0' + day;
+    }
+
+    return [year, month, day].join('-');
+}
 
 Date.prototype.sameDay = sameDay;
 Date.prototype.between = between;
+Date.prototype.getFormated = getFormated;
+Date.prototype.daysBetween = daysBetween;
--- vakansie-frontend/src/assets/util/dates.test.js
+++ vakansie-frontend/src/assets/util/dates.test.js
@@ -1,40 +1,40 @@
 import "./dates.js"
-import { Suite } from "../../test/test.js"
+import {Suite} from "../../test/test.js"
 
 const util = Suite("util-dates");
 
 util.add("equals", assert => {
 
-    const today = new Date();
-    assert.true(today.sameDay(today))
+    const day = new Date(2019, 5, 6);
+    assert.true(day.sameDay(day))
 
-    const tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
-    assert.true(!tomorrow.sameDay(today));
+    const tomorrow = new Date(day.getFullYear(), day.getMonth(), day.getDate() + 1);
+    assert.true(!tomorrow.sameDay(day));
 
-    const yesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() -1);
-    assert.true(!yesterday.sameDay(today));
+    const yesterday = new Date(day.getFullYear(), day.getMonth(), day.getDate() - 1);
+    assert.true(!yesterday.sameDay(day));
 });
 
 util.add("guard check", assert => {
 
-    const today = new Date();
+    const day = new Date(2019, 5, 6);
 
     try {
-        today.sameDay("null");
+        day.sameDay("null");
         assert.true(0) // should never arrive to this code!
     } catch (exception) {
         assert.true(exception);
     }
 
     try {
-        today.between("null", new Date());
+        day.between("null", new Date());
         assert.true(0) // should never arrive to this code!
     } catch (exception) {
         assert.true(exception);
     }
 
     try {
-        today.between(new Date(), false);
+        day.between(new Date(), false);
         assert.true(0) // should never arrive to this code!
     } catch (exception) {
         assert.true(exception);
@@ -43,13 +43,27 @@ util.add("guard check", assert => {
 
 util.add("between", assert => {
 
-    const today = new Date();
-    const yesterday = new Date(today.getFullYear(), today.getMonth(), today.getDate() -1);
-    const nextWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7);
+    const day = new Date(2019, 5, 6);
+    const yesterday = new Date(day.getFullYear(), day.getMonth(), day.getDate() - 1);
+    const nextWeek = new Date(day.getFullYear(), day.getMonth(), day.getDate() + 7);
 
-    assert.true(today.between(yesterday, nextWeek));
-    assert.true(!yesterday.between(today, nextWeek));
-    assert.true(!nextWeek.between(today, yesterday));
+    assert.true(day.between(yesterday, nextWeek));
+    assert.true(!yesterday.between(day, nextWeek));
+    assert.true(!nextWeek.between(day, yesterday));
 });
+
+util.add("count days between", assert => {
+
+    const day = new Date(2019, 5, 6);
+    const yesterday = new Date(day.getFullYear(), day.getMonth(), day.getDate() - 1);
+    const nextWeek = new Date(day.getFullYear(), day.getMonth(), day.getDate() + 7);
+    const lastYear = new Date(day.getFullYear() - 1, day.getMonth(), day.getDate());
+
+    assert.is(day.daysBetween(day), 0)
+    assert.is(day.daysBetween(yesterday), 1)
+    assert.is(day.daysBetween(nextWeek), 7)
+    assert.is(lastYear.daysBetween(day), 365)
+    assert.is(day.daysBetween(lastYear), 365)
+})
 
 util.run();
