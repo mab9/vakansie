57560ebf25cfeb281585430039a9a89d4bd39450
marc-antoine <marcantoine.bruelhart@gmail.com>
2021-01-02 15:39:35 +0100

    Add keycloak auth example
    - Add keycloak adapter js, keycloak.json
    - Add realm export for test purposes
    - Expand starter.js with simple keycloak auth logic
    - Add docker-compose to start keycloak container via  docker

--- vakansie-frontend/starter.js
+++ vakansie-frontend/starter.js
@@ -1,17 +1,38 @@
-
 import {LayoutController, LayoutView} from "./src/layout/layout.js";
 import {dom} from "./src/assets/util/dom.js";
+import "./keycloak.js"
 
-export { start } ;
+export {start} ;
 
 const start = (appRootId, persons) => {
 
     const CONTENT_WRAPPER = 'root';
-    const loggedIn = true;
-
     const layoutController = LayoutController();
 
-    const render = () => {
+    // https://github.com/keycloak/keycloak-documentation/blob/master/securing_apps/topics/oidc/javascript-adapter.adoc
+    const initKeyCloak = () => {
+        const keycloak = new Keycloak("./keycloak.json");
+        keycloak.init({
+            onLoad: 'login-required' // login-required will authenticate the client if the user is logged-in to vakansie or display the login page if not.
+        }).then(function (authenticated) {
+            // After the user is authenticated the application can make
+            // requests to RESTful services secured by vakansie by including the bearer token
+            // in the Authorization header.
+            console.info(authenticated ? 'authenticated' : 'not authenticated');
+            // keycloak.token
+
+            const jwtPayload = JSON.parse(atob(keycloak.token.split(".")[1]));
+            const userIdKeycloak = jwtPayload.sub;
+            const userName = jwtPayload.name;
+            const userEmail = jwtPayload.email;
+            render(authenticated);
+        }).catch(function () {
+            alert('failed to initialize');
+        });
+    }
+
+
+    const render = loggedIn => {
 
         // todo: think about resetting the model world on a possible re-render
 
@@ -20,12 +41,11 @@ const start = (appRootId, persons) => {
 
         if (loggedIn) {
             LayoutView(vakansie, layoutController);
+            root.replaceWith(vakansie);
         } else {
-            alert("not implemented at the moment")
+            initKeyCloak();
         }
-
-        root.replaceWith(vakansie);
     }
 
-    render();
+    render(false);
 }
